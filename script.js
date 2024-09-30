8a7422be1e9ff46f6b7deacdfa889a63f169fcb5
dlswife <131660051+dlswife@users.noreply.github.com>
2024-09-30 09:57:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+function init() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true
+    });
+
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        },
+        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    ScrollTrigger.refresh();
+}
+
+init();
+
+var crsr = document.querySelector(".cursor")
+var main = document.querySelector(".main")
+main.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+})
+
+
+gsap.from(".page1 h1,.page1 h2", {
+    y: 10,
+    rotate: 10,
+    opacity: 0,
+    delay: 0.3,
+    duration: 0.7
+})
+
+
+var tl = gsap.timeline ({
+    scrollTrigger: {
+        trigger: ".page1 h1",
+        scroller: ".main",
+        markers: true, // for debugging
+        start: "top 27%",
+        end: "top 0",
+        scrub: 3
+    }
+})
+
+tl.to(".page1 h1", {
+    x: -100, //vertical moving
+}, "anim");
+
+tl.to(".page1 h2", {
+    x: 100, //vertical moving
+}, "anim");
+
+tl.to(".page1 video", {
+    width:"90%",
+}, "anim")
+
+var tl2 = gsap.timeline ({
+    scrollTrigger: {
+        trigger: ".page1 h1",
+        scroller: ".main",
+        markers: true, // for debugging
+        start: "top -100%",
+        end: "top -110%",
+        scrub: 3
+    }
+})
+
+
+tl2.to(".main",{
+    backgroundColor:"#fff",
+
+})
+
